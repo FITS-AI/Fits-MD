31c3f2b0af1d58cb529bdd834599677a5e1277b8
Nurdin <mrsunnysummer@gmail.com>
2024-11-26 21:14:48 +0700

    chore: add ocr model

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,7 @@ android {
 
     buildFeatures{
         viewBinding = true
+        mlModelBinding = true
     }
 
     compileOptions {
@@ -49,6 +50,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.tensorflow.lite.support)
+    implementation(libs.tensorflow.lite.metadata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
