3af422f1b7bad21160c78e654e0faec601d9cc4c
Nurdin <mrsunnysummer@gmail.com>
2024-12-01 04:28:05 +0700

    feat: create register functionality

--- app/src/main/java/com/nudriin/fits/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/nudriin/fits/ui/register/RegisterActivity.kt
@@ -3,6 +3,8 @@ package com.nudriin.fits.ui.register
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
@@ -10,11 +12,17 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 import com.nudriin.fits.R
 import com.nudriin.fits.common.AuthViewModel
+import com.nudriin.fits.data.dto.user.UserLoginRequest
+import com.nudriin.fits.data.dto.user.UserSaveRequest
+import com.nudriin.fits.data.pref.UserModel
 import com.nudriin.fits.databinding.ActivityLoginBinding
 import com.nudriin.fits.databinding.ActivityRegisterBinding
+import com.nudriin.fits.ui.login.LoginActivity
 import com.nudriin.fits.ui.main.MainActivity
+import com.nudriin.fits.utils.Result
 import com.nudriin.fits.utils.ViewModelFactory
 
 class RegisterActivity : AppCompatActivity() {
@@ -42,6 +50,102 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         setupView()
+        setupAction()
+    }
+
+    fun setupAction() {
+        binding.nameEditText.addTextChangedListener {
+            validateName()
+        }
+
+        binding.emailEditText.addTextChangedListener {
+            validateEmail()
+        }
+
+        binding.passwordEditText.addTextChangedListener {
+            validatePassword()
+        }
+
+        binding.btnRegister.setOnClickListener {
+            val name = binding.nameEditText.text.toString()
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            viewModel.register(
+                UserSaveRequest(
+                    name = name,
+                    email = email,
+                    password = password
+                )
+            )
+                .observe(this) { result ->
+                    when (result) {
+                        is Result.Loading -> {
+                            binding.btnRegister.isEnabled = false
+                        }
+
+                        is Result.Success -> {
+                            Log.d("RegisterActivity", result.data.toString())
+                            binding.btnRegister.isEnabled = true
+                            startActivity(Intent(this, LoginActivity::class.java))
+                        }
+
+                        is Result.Error -> {
+                            binding.btnRegister.isEnabled = true
+                        }
+                    }
+                }
+        }
+    }
+
+    private fun validateName(): Boolean {
+        return when {
+            binding.nameEditText.text.toString().isEmpty() -> {
+                binding.nameEditTextLayout.error =
+                    getString(R.string.password_error_empty)
+                false
+            }
+
+            else -> {
+                binding.nameEditTextLayout.error = null
+                true
+            }
+        }
+    }
+
+    private fun validateEmail(): Boolean {
+        return when {
+            binding.emailEditText.text.toString().isEmpty() -> {
+                binding.emailEditTextLayout.error = getString(R.string.email_error_empty)
+                false
+            }
+
+            !Patterns.EMAIL_ADDRESS.matcher(binding.emailEditText.text.toString()).matches() -> {
+                binding.emailEditTextLayout.error =
+                    getString(R.string.email_error_invalid)
+                false
+            }
+
+            else -> {
+                binding.emailEditTextLayout.error = null
+                true
+            }
+        }
+    }
+
+    private fun validatePassword(): Boolean {
+        return when {
+            binding.passwordEditText.text.toString().isEmpty() -> {
+                binding.passwordEditTextLayout.error =
+                    getString(R.string.password_error_empty)
+                false
+            }
+
+            else -> {
+                binding.passwordEditTextLayout.error = null
+                true
+            }
+        }
     }
 
     private fun setupView() {
