2c56298ff0e10b1738f64bc9f57dc1cb0cbeaa55
Nurdin <mrsunnysummer@gmail.com>
2024-12-01 02:31:34 +0700

    feat: create logout functionality at profile page

--- app/src/main/java/com/nudriin/fits/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/nudriin/fits/ui/profile/ProfileFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.nudriin.fits.R
+import com.nudriin.fits.common.AuthViewModel
 import com.nudriin.fits.databinding.FragmentProfileBinding
+import com.nudriin.fits.utils.ViewModelFactory
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+
+    private val authViewModel: AuthViewModel by viewModels {
+        ViewModelFactory.getInstance(requireContext())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +25,17 @@ class ProfileFragment : Fragment() {
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding.btnLogout.setOnClickListener {
+            authViewModel.logout()
+        }
     }
 
 }
