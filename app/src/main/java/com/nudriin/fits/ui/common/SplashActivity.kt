082b88aa780d0286bc4a6b46b8ae06a184749fa2
Nurdin <mrsunnysummer@gmail.com>
2024-12-01 06:28:14 +0700

    feat: add splash screen

---
+++ app/src/main/java/com/nudriin/fits/ui/common/SplashActivity.kt
@@ -0,0 +1,40 @@
+package com.nudriin.fits.ui.common
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.nudriin.fits.databinding.ActivitySplashBinding
+import com.nudriin.fits.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupView()
+        Handler(Looper.getMainLooper()).postDelayed({
+            val splashIntent = Intent(this, MainActivity::class.java)
+            startActivity(splashIntent)
+            finish()
+        }, 2500)
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+
+}
