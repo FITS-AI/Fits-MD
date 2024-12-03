547e9d483539f615e274b54973195f51a4b9f667
Nurdin <mrsunnysummer@gmail.com>
2024-12-04 04:34:54 +0700

    chore: add isSelected field in allergy get all response

--- app/src/main/java/com/nudriin/fits/data/dto/allergy/AllergyResponse.kt
+++ app/src/main/java/com/nudriin/fits/data/dto/allergy/AllergyResponse.kt
@@ -4,24 +4,26 @@ import com.google.gson.annotations.SerializedName
 
 data class AllergyGetAllResponse(
 
-	@field:SerializedName("success")
-	val success: Boolean,
+    @field:SerializedName("success")
+    val success: Boolean,
 
-	@field:SerializedName("allergy")
-	val allergy: List<AllergyItem>,
+    @field:SerializedName("allergy")
+    val allergy: List<AllergyItem>,
 
-	@field:SerializedName("message")
-	val message: String
+    @field:SerializedName("message")
+    val message: String
 )
 
 data class AllergyItem(
 
-	@field:SerializedName("allergen")
-	val allergen: String,
+    @field:SerializedName("allergen")
+    val allergen: String,
 
-	@field:SerializedName("id")
-	val id: Int,
+    @field:SerializedName("id")
+    val id: Int,
 
-	@field:SerializedName("allergy_name")
-	val allergyName: String
+    @field:SerializedName("allergy_name")
+    val allergyName: String,
+
+    var isSelected: Boolean = false
 )
