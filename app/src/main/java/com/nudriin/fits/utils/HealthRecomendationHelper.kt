2bb10071d79883caeed14b010232e3935092485a
Nurdin <mrsunnysummer@gmail.com>
2024-12-07 04:13:24 +0700

    chore: create HealthRecommendation summary

--- app/src/main/java/com/nudriin/fits/utils/HealthRecomendationHelper.kt
+++ app/src/main/java/com/nudriin/fits/utils/HealthRecomendationHelper.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import com.google.android.gms.tflite.client.TfLiteInitializationOptions
 import com.google.android.gms.tflite.gpu.support.TfLiteGpu
 import com.google.android.gms.tflite.java.TfLite
+import com.nudriin.fits.data.domain.HealthRecommendationSummary
 import org.tensorflow.lite.InterpreterApi
 import org.tensorflow.lite.gpu.GpuDelegateFactory
 import java.io.FileInputStream
@@ -106,6 +107,77 @@ class HealthRecomendationHelper(
                 return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
             }
         }
+    }
+
+    fun recommendationSummary(
+        grade: String,
+        isDiabetes: Boolean = false
+    ): HealthRecommendationSummary {
+        val healthRecommendationSummary: HealthRecommendationSummary
+        when (grade) {
+            (grade == "A").toString() -> {
+                healthRecommendationSummary = HealthRecommendationSummary(
+                    "Healthy Food",
+                    "Excellent for health. This food is nutrient-dense and can help maintain weight and heart health",
+                    "Low (less than 5g per serving)",
+                    "Low (less than 5g per serving, with minimal saturated fat)",
+                    "High (more than 15g per serving)",
+                    "Moderate (200-300 calories per serving)",
+                    warning = if (isDiabetes) "Note: This food is suitable for individuals with diabetes due to its low sugar content." else ""
+                )
+            }
+
+            (grade == "B").toString() -> {
+                healthRecommendationSummary = HealthRecommendationSummary(
+                    "Good Food",
+                    "Good for health. This food can be part of a balanced diet, although portion size and frequency of consumption should be monitored",
+                    "Moderate (5-10g per serving)",
+                    "Moderate (5-10g per serving, with controlled saturated fat)",
+                    "Moderate (10-15g per serving)",
+                    "Moderate (300-400 calories per serving)",
+                    warning = if (isDiabetes) "Note: Individuals with diabetes should consume this food in moderation due to its moderate sugar content." else ""
+                )
+            }
+
+            (grade == "C").toString() -> {
+                healthRecommendationSummary = HealthRecommendationSummary(
+                    "Fair Food",
+                    "Should be consumed with caution. This food can be an occasional choice, but it should not be a mainstay in daily diet",
+                    "High (10-20g per serving)",
+                    "High (10-20g per serving, with some saturated fat)",
+                    "Low (less than 10g per serving)",
+                    "High (400-600 calories per serving)",
+                    warning = if (isDiabetes) "Warning: This food contains high sugar levels and is not recommended for individuals with diabetes." else ""
+                )
+            }
+
+            (grade == "D").toString() -> {
+                healthRecommendationSummary = HealthRecommendationSummary(
+                    "Unhealthy Food",
+                    "Not recommended for regular consumption. This food can contribute to health issues such as obesity, diabetes, and heart disease",
+                    "Very high (more than 20g per serving)",
+                    "Very high (more than 20g per serving, with high saturated fat)",
+                    "Very low (less than 5g per serving)",
+                    "Very high (more than 600 calories per serving)",
+                    warning = if (isDiabetes) "Warning: This food is extremely high in sugar and is unsafe for individuals with diabetes." else ""
+                )
+            }
+
+            else -> {
+                healthRecommendationSummary = HealthRecommendationSummary(
+                    "Not Applicable",
+                    "The health grade for this food item cannot be determined at the moment. This might be due to incomplete or insufficient data.",
+                    "Not available",
+                    "Not available",
+                    "Not available",
+                    "Not available",
+                    warning = if (isDiabetes) "Not available" else "Not available"
+                )
+            }
+
+
+        }
+        return healthRecommendationSummary
     }
 
     companion object {
